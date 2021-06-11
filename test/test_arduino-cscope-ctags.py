3b5f15ab6a1bfa469ab7d5a27061317c6478dc8c
ifTNT <iftnt1999@gmail.com>
2021-06-12 01:56:01 +0800

    Testing with python module instead of command-line

--- test/test_arduino-cscope-ctags.py
+++ test/test_arduino-cscope-ctags.py
@@ -3,15 +3,32 @@
 import subprocess
 from pathlib import Path
 import shutil
+import importlib
+import sys
+
+# Helper function to load python module from file name
+def import_from_path(path):
+    module_name = Path(path).name.replace('-', '_')
+    spec = importlib.util.spec_from_loader(
+        module_name,
+        importlib.machinery.SourceFileLoader(module_name, path)
+    )
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    sys.modules[module_name] = module
+    return module
+
+# Instanced the main object
+ccino = import_from_path("./arduino-cscope-ctags")
 
 test_sketch_path = Path("./test/FreeRTOS_ESP32").absolute()
 
 def test_main_default():
-  arduino_cli = subprocess.run([
-    "./arduino-cscope-ctags",
+  ccino.main(
     "esp32:esp32:nodemcu-32s",
-    str(test_sketch_path)
-  ])
+    str(test_sketch_path),
+    ".tags"
+  )
 
   tags_path = test_sketch_path/".tags"
 
@@ -25,13 +42,11 @@ def test_main_default():
   shutil.rmtree(tags_path)
 
 def test_main_custom_output():
-  arduino_cli = subprocess.run([
-    "./arduino-cscope-ctags",
-    "-o",
-    ".tags/custom/",
+  ccino.main(
     "esp32:esp32:nodemcu-32s",
-    str(test_sketch_path)
-  ])
+    str(test_sketch_path),
+    ".tags/custom/"
+  )
 
   tags_path = test_sketch_path/".tags/custom"
 
